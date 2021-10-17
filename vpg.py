d3c3a0f5283d967926c5ec9d850537edaf4c0146
Kakooee Reza rkakooee <reza.kakooee@hslu.ch>
2021-10-17 21:13:36 +0200

    VPG works correctly with reward-to-go

--- vpg.py
+++ vpg.py
@@ -5,6 +5,9 @@ Created on Sun Oct 10 11:20:44 2021
 @author: Reza Kakooee
 """
 # %%
+import time
+import numpy as np
+
 import torch
 import torch.nn as nn
 from torch.distributions.categorical import Categorical
@@ -69,7 +72,8 @@ class VPG:
         
     def select_action(self, obs):
         pi = self.get_policy(obs)
-        return pi.sample().item()
+        action =  pi.sample().item()
+        return action
     
     def compute_loss(self, obs, act, rew):
         dist = self.get_policy(obs)
@@ -93,32 +97,36 @@ class PGLearner:
     def __init__(self, env, config):
         self.env = env
         self.config = config
-        self.batch_size = config['model_config']['batch_size']
+        self.batch_size = config['batch_size']
+        self.discount_factor = config['discount_factor']
         
-    def calculate_return(self, rewards):
-        sum_reward = 0
-        discounted_rewards = []
-        rewards.reverse()
-        for r in rewards:
-          sum_reward = r + self.dicount_factor*sum_reward
-          discounted_rewards.append(sum_reward)
-        discounted_rewards.reverse()
-        return discounted_rewards   
+        self.agent = VPG(config)
     
-    def interact(self):
+    
+    def learn(self, n_epochs):
+         for i in range(n_epochs):
+             print(f"- - - - - - - - - - - - - - - - - - - - - - - - - Epoch {i}")
+             batch_obss, batch_acts, batch_gains = self._interact()
+             self.agent.train_one_epoch(batch_obss, batch_acts, batch_gains)
+    
+             
+    def _interact(self):
         batch_obss = []
         batch_acts = []
         batch_rets = []
         batch_lens = []
         batch_rews = []
+        batch_mean_rews = []
         batch_gains = []
         
-        while True:
-            
+        count_ep = 0
+        while True: # for batch_size            
             done = False
             obs = self.env.reset()
             episode_reward = []
+            i = 1
             while not done:
+                
                 action = self.agent.select_action(obs)
                 next_obs, reward, done, info = self.env.step(action)
                 
@@ -128,34 +136,73 @@ class PGLearner:
                 episode_reward.append(reward)
                 
                 obs = next_obs.copy()
+                i += 1
+                
             
-            batch_lens.append(len(reward))
-            batch_rets.append(sum(episode_reward))            
+            count_ep += 1
+            
+            batch_lens.append(len(episode_reward))
+            batch_rets.append( sum([ self.discount_factor**(i-1) * r for i, r in enumerate(episode_reward) ]) )
+            episode_mean_rews = np.mean(episode_reward)
+            batch_mean_rews.append(episode_mean_rews)
+            rew2go = self._calculate_reward_to_go(episode_reward)
+            batch_gains += rew2go
+            
+            print(f"----- Episode: {count_ep}, EpisodeLen: {i}, EpisodeMeanReward: {episode_mean_rews}")
+            
             if len(batch_acts) >= self.batch_size:
                 break
         
-        return batch_obss, batch_acts, batch_rews
-                
-                
-    def learn(self, n_epochs):
-         for i in range(n_epochs):
-             batch_obss, batch_acts, batch_rews = self.interact()
-             self.train_one_epoch(batch_obss, batch_acts, batch_rews)
-             
+        print(f"********** This batch contains {count_ep} episodes.")
+        return batch_obss, batch_acts, batch_gains
+    
+
+    def _calculate_reward_to_go(self, rewards):
+        sum_reward = 0
+        discounted_rewards = []
+        rewards.reverse()
+        for r in rewards:
+          sum_reward = r + self.discount_factor*sum_reward
+          discounted_rewards.append(sum_reward)
+        discounted_rewards.reverse()
+        return discounted_rewards   
+
+            
+    def inference(self):
+        done = False
+        obs = self.env.reset()
+        reward_list = []
+        episode_reward = []
+        i = 0
+        while not done:
+            action = self.agent.select_action(obs)
+            next_obs, reward, done, info = self.env.step(action)
+            
+            episode_reward.append(reward)
+            env.render()
+            obs = next_obs.copy()
+            time.sleep(0.5)
+            i += 1
+        reward_list.append(sum(episode_reward))
+        
+        print(f",,,,,,,, Inference Episode Len: {i}")        
+        print(f".................... Inference Reward: {reward_list}")
+        
 # %%
 class VPGConfig:
     def __init__(self, env):
         self.env = env
         
         self.model_config = {
-            'obs_dim': env.observation_space.shape,
+            'obs_dim': env.observation_space.shape[0],
             'n_actions': env.action_space.n,
-            'hiden_sizes': [32 , 32],
+            'hiden_sizes': [32, 32],
             'input_activation': nn.Tanh,
             'hiden_activation': nn.ReLU,
-            'output_activation': nn.Softmax,
+            'output_activation': nn.Identity, #nn.Softmax,
             }
         
+        self.batch_size = 200
         self.discount_factor = 0.99
         
     
@@ -171,9 +218,9 @@ if __name__ == "__main__":
     config = VPGConfig(env).get_config()
     
     learner = PGLearner(env, config)
-    learner.learn(n_epochs=1)
-    
-    
+    learner.learn(n_epochs=1000)
+    learner.inference()
+    env.close()
     
         
         
